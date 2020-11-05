975a2f886729a05513f2e9d96a881cdf7c5374aa
HallexCosta <hallex.costa@hotmail.com>
2020-11-04 23:54:41 -0300

    Add routes

---
+++ src/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+)
+
+func Routes() {
+	app.Get("/", func(context *fiber.Ctx) {
+		User := new(struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Password string `json:"password"`
+			Age      int    `json:"age"`
+		})
+
+		context.BodyParser(User)
+
+		// Destructuring
+		Name, Email, Password, Age := User.Name, User.Email, User.Password, User.Age
+		fmt.Printf("User {\n  name: %s\n  email: %s\n  password: %s\n  age: %x\n}\n", Name, Email, Password, Age)
+
+		context.JSON(fiber.Map{
+			"data":    User,
+			"success": true,
+			"message": "Current Context Success",
+		})
+	})
+}
