6c6ad37891351bed237c6299572c4939069e043d
HallexCosta <hallex.costa@hotmail.com>
2020-11-08 14:35:48 -0300

    Add comment on fucntions Helpers

--- src/helpers/Helpers.go
+++ src/helpers/Helpers.go
@@ -2,13 +2,25 @@ package helpers
 
 import file "crud-without-database-golang/src/helpers/File"
 
+// Persistence ...
+type Persistence interface {
+	CreateFile(filename string)
+	ReadFile(filename string)
+	WriteFile(filename string, content string)
+}
+
+// CreateFile ...
 func CreateFile(filename string) {
 	file.Create(filename)
 }
+
+// ReadFile ...
 func ReadFile(filename string) string {
 	b, _ := file.Read(filename)
 	return string(b[:])
 }
+
+// WriteFile ...
 func WriteFile(filename string, content string) {
 	file.Write(filename, []byte(content))
 }
