bec949ec40f79b0d6867572881cd98171bf044ea
HallexCosta <hallex.costa@hotmail.com>
2020-11-08 12:57:21 -0300

    Initializing persistence in json file

--- src/main.go
+++ src/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "crud-without-database-golang/src/config"
+
+func init() {
+	_AppConfig := config.NewAppConfig()
+	_Persistence := _AppConfig.NewPersistence("users")
+
+	_Persistence.InitPersistence()
+}
+
 func main() {
 	App()
 	Routes()
