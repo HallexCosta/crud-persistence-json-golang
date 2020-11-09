f7a4a0832bd534cb8afbad94a0f4936215933883
HallexCosta <hallex.costa@hotmail.com>
2020-11-09 19:45:42 -0300

    Add types of User Features

---
+++ src/feature/TypesUserRepository.go
@@ -0,0 +1,9 @@
+package feature
+
+import "crud-without-database-golang/src/repository"
+
+// UserRepositoryInterface ...
+type UserRepositoryInterface = repository.UserRepositoryInterface
+
+// UserRepository ...
+type UserRepository = repository.UserRepository
