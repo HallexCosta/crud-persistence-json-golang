dff0027cfe947453f6a095822999cd994518decb
HallexCosta <hallex.costa@hotmail.com>
2020-11-09 18:11:48 -0300

    Adding feature ListUsers in route /users -> GET

--- src/routes.go
+++ src/routes.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/gofiber/fiber/v2"
+	"crud-without-database-golang/src/feature"
 )
 
+// Routes ...
 func Routes() {
-	app.Get("/", func(context *fiber.Ctx) error {
-		User := new(struct {
-			Name     string `json:"name"`
-			Email    string `json:"email"`
-			Password string `json:"password"`
-			Age      int    `json:"age"`
-		})
+	app.Get("/users", feature.ListUsers)
 
-		context.BodyParser(User)
+	// app.Get("/", func(context *fiber.Ctx) error {
+	// 	User := new(struct {
+	// 		Name     string `json:"name"`
+	// 		Email    string `json:"email"`
+	// 		Password string `json:"password"`
+	// 		Age      int    `json:"age"`
+	// 	})
 
-		// Destructuring
-		Name, Email, Password, Age := User.Name, User.Email, User.Password, User.Age
-		fmt.Printf("User {\n  name: %s\n  email: %s\n  password: %s\n  age: %x\n}\n", Name, Email, Password, Age)
+	// 	context.BodyParser(User)
 
-		return context.JSON(fiber.Map{
-			"data":    User,
-			"success": true,
-			"message": "Current Context Success",
-		})
-	})
+	// 	// Destructuring
+	// 	Name, Email, Password, Age := User.Name, User.Email, User.Password, User.Age
+	// 	fmt.Printf("User {\n  name: %s\n  email: %s\n  password: %s\n  age: %x\n}\n", Name, Email, Password, Age)
+
+	// 	return context.JSON(fiber.Map{
+	// 		"data":    User,
+	// 		"success": true,
+	// 		"message": "Current Context Success",
+	// 	})
+	// })
 }
