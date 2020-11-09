a23eef3f263619d93438f9c56fbebb8651820a10
HallexCosta <hallex.costa@hotmail.com>
2020-11-09 18:10:55 -0300

    Declared init function for configure AppConfig & Persistence in JSON file

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import "crud-without-database-golang/src/config"
 
 func init() {
-	_AppConfig := config.NewAppConfig()
+	_AppConfig := config.ImportAppConfig()
 	_Persistence := _AppConfig.NewPersistence("users")
 
 	_Persistence.InitPersistence()
