c21d071aae545d67ad152e5b76e49ae5d386e076
HallexCosta <hallex.costa@hotmail.com>
2020-11-10 00:36:02 -0300

    Add route for list user specific and remove removing commented code

--- src/routes.go
+++ src/routes.go
@@ -7,28 +7,8 @@ import (
 // Routes ...
 func Routes() {
 	app.Get("/users", feature.ListUsers)
+	app.Get("/users/:id", feature.ListUser)
 	app.Post("/users", feature.CreateUser)
 	app.Put("/users/:id", feature.EditUser)
 	app.Delete("/users/:id", feature.DeleteUser)
-
-	// app.Get("/", func(context *fiber.Ctx) error {
-	// 	User := new(struct {
-	// 		Name     string `json:"name"`
-	// 		Email    string `json:"email"`
-	// 		Password string `json:"password"`
-	// 		Age      int    `json:"age"`
-	// 	})
-
-	// 	context.BodyParser(User)
-
-	// 	// Destructuring
-	// 	Name, Email, Password, Age := User.Name, User.Email, User.Password, User.Age
-	// 	fmt.Printf("User {\n  name: %s\n  email: %s\n  password: %s\n  age: %x\n}\n", Name, Email, Password, Age)
-
-	// 	return context.JSON(fiber.Map{
-	// 		"data":    User,
-	// 		"success": true,
-	// 		"message": "Current Context Success",
-	// 	})
-	// })
 }
